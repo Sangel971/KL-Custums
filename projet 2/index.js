c66f19395703071b8437aa77c4ef6f4e31d0ea68
Sangel971 <steven.8@outlook.fr>
2024-09-09 23:13:04 +0200

    Delete projet 2 directory

--- projet 2/index.js
+++
@@ -1,6 +0,0 @@
-const sideBar = document.getElementById("side-bar");
-const bugger = document.querySelector(".toggle-btn");
-
-bugger.addEventListener("click", () => {
-  sideBar.classList.toggle("side-bar");
-});
